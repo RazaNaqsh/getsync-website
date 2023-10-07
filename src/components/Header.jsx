2e2aaa84bf683d84fe9d815de56c2984b7bb0977
RazaNaqsh <owaisraza1704@gmail.com>
2023-10-07 10:05:22 +0530

    Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import bg from "../assets/bg.png";
 
 const Header = () => {
   return (
-    <div className="h-[500px] bg-gradient-to-b from-[#BF82C9] to-[#9F52ED]">
+    <div className="relative">
+      <div className="absolute -z-10 w-full">
+        <img src={bg} className="w-full h-[600px]" alt="" />
+      </div>
       <Navbar />
       <Hero />
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const tabs = ["Home", "About", "Events", "Services", "Leadership"];
+  const [activeTab, setActiveTab] = useState("Home");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  return (
+    <nav>
+      <div className="w-[85%] mx-auto flex px-4 py-6 justify-between items-end">
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div>
+          <ul className="flex text-lg font-sans  gap-10">
+            {tabs.map((tab, index) => (
+              <li
+                key={index}
+                className={`cursor-pointer ${
+                  activeTab === tab ? "text-white" : "text-black"
+                }`}
+                onClick={() => handleTabClick(tab)}
+              >
+                {tab}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
