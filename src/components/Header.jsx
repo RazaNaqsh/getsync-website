23d39be1f13c2f60bae43d950dffe206d3152ac8
RazaNaqsh <owaisraza1704@gmail.com>
2023-10-08 14:25:49 +0530

    Position adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,19 +9,19 @@ import share from "../assets/share.png";
 const Header = () => {
   return (
     <div className="relative">
-      <div className="relative h-[600px] overflow-hidden">
+      <div className="relative h-[700px] overflow-hidden">
         <div className="absolute -z-10 w-full">
-          <img src={bg} className="w-full h-[600px]" alt="" />
+          <img src={bg} className="w-full h-[700px]" alt="" />
         </div>
         <Navbar />
         <Hero />
-        <img src={cup} className="absolute left-0 top-16" alt="" />
+        <img src={cup} className="absolute left-0 top-28" alt="" />
         <img
           src={line}
-          className="absolute translate-x-[285px] -translate-y-[390px] w-[69%]"
+          className="absolute translate-x-[285px] -translate-y-[340px] w-[69%]"
           alt=""
         />
-        <img src={tie} className="absolute right-0 top-40" alt="" />
+        <img src={tie} className="absolute right-0 top-52" alt="" />
       </div>
       <button className="absolute -bottom-7 py-3 translate-x-[680px] w-[350px] rounded-full  bg-white border-2 border-t-[3px] border-black border-r-4 border-b-[6px] font-bold text-lg flex justify-center gap-8 items-center">
         <p>JOIN NOW</p>
