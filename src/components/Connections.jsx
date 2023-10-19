0d41a00d5a0989562f5d74113c85aa526e1c109d
RazaNaqsh <owaisraza1704@gmail.com>
2023-10-19 14:29:03 +0530

    Connections component done

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import du from "../assets/du.png";
+import cu from "../assets/cu.png";
+import su from "../assets/su.png";
+import au from "../assets/au.png";
+import aktu from "../assets/aktu.png";
 
 const Connections = () => {
   return (
     <div className="py-10">
       <h1 className="font-sedan text-[48px] text-center">OUR CONNECTIONS</h1>
+      <div className="flex justify-around w-[90%] mx-auto my-12">
+        <img src={du} alt="" />
+        <img src={cu} alt="" />
+        <img src={su} alt="" />
+        <img src={au} alt="" />
+        <img src={aktu} alt="" />
+      </div>
     </div>
   );
 };
