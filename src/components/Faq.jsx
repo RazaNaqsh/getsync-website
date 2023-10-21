9b0af380feb6807069203c6ecf0a58f56be8abac
RazaNaqsh <owaisraza1704@gmail.com>
2023-10-21 22:35:47 +0530

    Accordion added

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 
 const Faq = () => {
+  const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  const content = [
+    {
+      key: 1,
+      title: "1. What is a Community?",
+      desc: "A community is a place or platform where people of same genre meets community is a place or platform where people of same genre meets community is a place or platform where people of same genre meetsA community is a place or platform where people of same genre meets",
+    },
+    {
+      key: 2,
+      title: "2. What is getSync?",
+      desc: "A community is a place or platform where people of same genre meets community is a place or platform where people of same genre meets community is a place or platform where people of same genre meetsA community is a place or platform where people of same genre meets",
+    },
+    {
+      key: 3,
+      title: "3. How I will be benifited?",
+      desc: "A community is a place or platform where people of same genre meets community is a place or platform where people of same genre meets community is a place or platform where people of same genre meetsA community is a place or platform where people of same genre meets",
+    },
+    {
+      key: 4,
+      title: "4. Is it free?",
+      desc: "A community is a place or platform where people of same genre meets community is a place or platform where people of same genre meets community is a place or platform where people of same genre meetsA community is a place or platform where people of same genre meets",
+    },
+  ];
   return (
     <div className="py-10">
       <h1 className="font-sedan text-[48px] text-center">FREQUENTLY ASKED</h1>
+      <div className="w-[80%] mx-auto mt-10 mb-10">
+        <Accordion variant="splitted">
+          {content.map((c) => (
+            <AccordionItem
+              key={c.key}
+              style={{
+                marginBottom: "40px",
+                border: "2px solid black",
+                fontSize: "20px",
+                minHeight: "77px",
+                fontWeight: "700",
+                paddingTop: "4px",
+              }}
+              title={c.title}
+            >
+              <p className="px-8 pb-6 font-medium">{c.desc}</p>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -11,5 +18,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
