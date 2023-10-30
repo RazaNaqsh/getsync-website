3b16fd09fba5a4a48f80a3e691d80a473ca828dd
RazaNaqsh <owaisraza1704@gmail.com>
2023-10-30 17:08:19 +0530

    Footer feedback card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,42 @@ import yt from "../assets/yt.svg";
 import tw from "../assets/tw.svg";
 
 const Footer = () => {
+  const submit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="relative  bg-[#232323] flex items-center w-full gap-24 py-[64px] px-[108px] text-white">
-      <div className="h-[540px] w-[400px] bg-[#000000] rounded-[16px] p-[30px]"></div>
+      <div className="h-[540px] w-[400px] bg-[#000000] rounded-[16px] p-[30px]">
+        <h3 className="text-[#FFED48] text-[10px] font-helvetica font-light">
+          FEEDBACK
+        </h3>
+        <h1 className="font-helvetica text-[28px] leading-7 my-6">
+          Have any Feedback or Query?
+        </h1>
+        <form onSubmit={submit} className="space-y-5">
+          <input
+            type="text"
+            className="bg-transparent border-[1px] text-[12px] w-[340px] h-[56px] py-[8px] px-[12px] rounded-[12px]"
+            placeholder="FULL NAME"
+          />
+          <input
+            type="email"
+            className="bg-transparent border-[1px] text-[12px] w-[340px] h-[56px] py-[8px] px-[12px] rounded-[12px]"
+            placeholder="FULL NAME"
+          />
+          <input
+            type="text"
+            className="bg-transparent border-[1px] text-[12px] w-[340px] h-[97px] py-[8px] px-[12px] rounded-[12px]"
+            placeholder="MESSAGE"
+          />
+          <button className="border-[1px] border-b-[3px] border-r-[2px] border-white rounded-[50px] py-[10px] px-[30px]  text-[14px] bg-[#7B209B] text-white">
+            Send
+          </button>
+        </form>
+        <p className="text-[10px] opacity-40 font-helvetica mt-5 ml-2">
+          Privacy
+        </p>
+      </div>
       <div className="w-[70%]  h-[460px] p-4">
         <div className="flex justify-between items-start">
           <div>
